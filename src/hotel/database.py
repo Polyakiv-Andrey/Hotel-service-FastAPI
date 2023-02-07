e2dd132d35d1feff86704cc33469a23d708d47f6
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-02-07 20:39:07 +0200

    Created hotel table and country table in database

--- src/hotel/database.py
+++ src/hotel/database.py
@@ -6,7 +6,7 @@ from .settings import settings
 
 engine = create_engine(
     settings.database_url,
-    connect_args={"check_same_tread": False},
+    connect_args={"check_same_thread": False},
 )
 
 Session = sessionmaker(
